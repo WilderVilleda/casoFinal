0de1f8f97ad8782ed1acc656ef35b4928c142560
WilderVilleda <148466596+WilderVilleda@users.noreply.github.com>
2023-11-22 22:38:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,39 @@ fecha_final = datetime.date.today()
 fecha_inicial = fecha_final - datetime.timedelta(days=365 * 3)
 data = yf.download(stocks, start=fecha_inicial, end=fecha_final)["Adj Close"]
 
+
+#Portafolio_promedio_simple
+returns = data.pct_change()
+ret_acumulado = (1+returns).cumprod()
+pesos = np.array([1/6, 1/6,1/6,1/6,0,1/6,1/6])
+returns["Portafolio_promedio_simple"] = returns.dot(pesos)
+ret_acumulado_2 = (1+returns).cumprod()
+
+#Portafolio_max_Sharpe
+pesos1 = np.array([0, 0,0.4713360636886999,0.5286639363113002,0,0,0])
+returns2 = data.pct_change()
+ret_acumulado_3 = (1+returns2).cumprod()
+returns2["Portafolio_maximo_sharpe"]=returns2.dot(pesos1)
+ret_acumulado_4 = (1+returns2).cumprod()
+
+#Portafolio_min_Volatilidad
+pesos2 = np.array([0.0090749879390336, 0.1894368675809966,0.1211972087037164,0.2512103107260549,0.2418873547154737,0.0854810739198495,
+                0.1017121964148752])
+returns3 = data.pct_change()
+ret_acumulado_5 = (1+returns3).cumprod()
+returns3["Portafolio_minima_varianza"]=returns3.dot(pesos2)
+ret_acumulado_6 = (1+returns3).cumprod()
+
+#Nuevo dataframe con los retornos acumulados
+
+columna_portafolio_2 = ret_acumulado_6["Portafolio_minima_varianza"]
+columna_portafolio_1 = ret_acumulado_4["Portafolio_maximo_sharpe"]
+columna_portafolio = ret_acumulado_2["Portafolio_promedio_simple"]
+
+conjunto_datos = pd.concat([columna_portafolio_2, columna_portafolio_1, columna_portafolio], axis=1)
+
+conjunto_datos.reset_index(inplace=True)
+
 # Crear aplicación Dash
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server=app.server
