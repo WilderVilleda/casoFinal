d2a2279fe5c03097e2091751010fb0e4bba4b26c
WilderVilleda <148466596+WilderVilleda@users.noreply.github.com>
2023-11-22 21:40:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def actualizar_grafico(selected_accion, selector, fechas):
     return fig, graph_ms, graph_mv
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host="0.0.0.0", port=70000)
+    app.run_server(debug=False, host="0.0.0.0", port=10000)
